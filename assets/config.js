662afda5289d4a4e92d75d6ea43108f7a45e32ab
jarieljvc15-jpg <jarieljvc15@gmail.com>
2026-05-20 08:02:47 +0800

    Update config.js

--- assets/config.js
+++ assets/config.js
@@ -1,5 +1,5 @@
 // Replace the value below with your deployed Google Apps Script Web App URL.
 // Deploy instructions: see SETUP.md
 const CONFIG = {
-  APPS_SCRIPT_URL: "YOUR_APPS_SCRIPT_URL"
+  APPS_SCRIPT_URL: "https://script.google.com/macros/s/AKfycbxSNTy-F9JLicgbiaDH0UZa_DwmHGxgxi8cJiiL7sGUBKeJcwK4Cyxgzoal40hrq1Ck/exec"
 };
